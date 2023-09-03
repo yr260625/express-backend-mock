aa695103117a7256eaf7a0c8f9d7b7d086bf284a
yr260625 <yr260625.learning@gmail.com>
2023-09-03 11:47:23 +0900

    [ADD] env

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,16 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
+// ルーティング設定
+app.use('/', Router);
+
 // DB接続
 connectDB
   .initialize()
   .then(() => {
-    app.use('/', Router);
     // サーバ起動
-    const port = 3000;
-    app.listen(port);
-    console.log('listen on port ' + port);
+    app.listen(process.env.PORT);
+    console.log('listen on port ' + process.env.PORT);
   })
   .catch((error) => {
     console.error(`Data Source initialization error`, error);
--- src/data-source.ts
+++ src/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export const connectDB = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'cQ6Iptj3jthmtWpd',
-  database: 'express_backend_mock',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: false,
   logging: false,
   entities: ['src/entities/*.ts'],
